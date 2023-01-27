41e385b35c158a79237e85b045ed8bc4216294c3
Bhumika Kanyal <79444139+Bhumika79@users.noreply.github.com>
2023-01-27 09:33:15 +0530

    Update wait-command.c

--- wait-command.c
+++ wait-command.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include  <stdio.h>
 #include  <string.h>
 #include  <unistd.h>
@@ -25,4 +17,3 @@ void main()
     printf("\nvalue of i in parent process is %d", i);
     }
 }
-
